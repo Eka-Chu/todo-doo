fe094e4178ce1d9c92c3b56381eb01832b294376
Eka-Chu <echukhonseva@gmail.com>
2023-06-14 15:17:03 +0400

    filter

--- src/app.tsx
+++ src/app.tsx
@@ -5,9 +5,11 @@ import TaskCounter from './components/TaskCounter';
 import TodoModel from './models/TodoModel';
 import { mockData } from './mockData';
 import AddTodo from './components/AddTodo';
+import './style.css';
 
 const App: React.FC = () => {
   const [todos, setTodos] = useState<TodoModel[]>(mockData);
+  const [filter, setFilter] = useState('all'); // State for the filter option
 
   const handleDeleteTodo = (id: string) => {
     setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
@@ -31,17 +33,50 @@ const App: React.FC = () => {
     );
   };
 
+  const handleFilter = (filter: string) => {
+    setFilter(filter);
+  };
+
+  const filteredTodos = todos.filter((todo) => {
+    if (filter === 'active') {
+      return !todo.completed;
+    } else if (filter === 'completed') {
+      return todo.completed;
+    } else {
+      return true;
+    }
+  });
+
   const completedCount = todos.filter((todo) => todo.completed).length;
 
   return (
     <div>
       <Header />
       <AddTodo onAdd={handleAddTodo} />
-      <TodoList todos={todos} onDelete={handleDeleteTodo} onSave={handleSave} />
+      <div>
+        <button
+          onClick={() => handleFilter('all')}
+          className={filter === 'all' ? 'active' : ''}
+        >
+          Show All Tasks
+        </button>
+        <button
+          onClick={() => handleFilter('active')}
+          className={filter === 'active' ? 'active' : ''}
+        >
+          Show Active Tasks
+        </button>
+        <button
+          onClick={() => handleFilter('completed')}
+          className={filter === 'completed' ? 'active' : ''}
+        >
+          Show Completed Tasks
+        </button>
+      </div>
+      <TodoList todos={filteredTodos} onDelete={handleDeleteTodo} onSave={handleSave} />
       <TaskCounter todos={todos} completedCount={completedCount} />
     </div>
   );
 };
 
 export default App;
-
--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,3 +1,32 @@
+// import React, { useState } from 'react';
+
+// interface AddTodoProps {
+//   onAdd: (title: string) => void;
+// }
+
+// const AddTodo: React.FC<AddTodoProps> = ({ onAdd }) => {
+//   const [title, setTitle] = useState('');
+
+//   const handleAddTodo = () => {
+//     if (title.trim() !== '') {
+//       onAdd(title.trim());
+//       setTitle('');
+//     }
+//   };
+
+//   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     setTitle(event.target.value);
+//   };
+
+//   return (
+//     <div>
+//       <input type="text" value={title} onChange={handleInputChange} />
+//       <button onClick={handleAddTodo}>Add</button>
+//     </div>
+//   );
+// };
+
+// export default AddTodo;
 import React, { useState } from 'react';
 
 interface AddTodoProps {
